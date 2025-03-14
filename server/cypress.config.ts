644704e8e3befb0183ba991b572d195944cdd546
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-03-13 21:26:37 -0700

    implemt node config listeners

--- server/cypress.config.ts
+++ server/cypress.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
       framework: "react",
       bundler: "vite",
     },
-    specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+    // specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
